82eaf3263f015b78455d4f1ff14f242abe14c8b5
AllenCHM <chengchuanming@hotmail.com>
2015-12-12 00:13:33 +0800

    增加zol笑话数据库

--- test.py
+++ test.py
@@ -7,7 +7,18 @@ __author__ = 'AllenCHM'
 import time
 import hashlib
 from flask import  Flask, request, make_response
+import pymongo
+import xml.etree.ElementTree as ET
+
+
 app = Flask(__name__)
+
+def getContent(fromu):
+    conn = pymongo.MongoClient()
+    db = conn[u'wJoke']
+    doc = db[u'wJoke']
+    tmp = doc.find_one_and_update({u'receiver':{u'$nin':[fromu,]}},{u'$addToSet':{u'receiver':fromu}}, {u'content':1})
+    return tmp[u'content']
 
 
 @app.route('/auth', methods=['GET', 'POST'])
@@ -24,6 +35,19 @@ def wechat_auth():
         s = ''.join(s)
         if (hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
+
+    if request.method == 'POST':
+        rec = request.stream.read()
+        xml_rec = ET.fromstring(rec)
+        tou = xml_rec.find('ToUserName').text
+        fromu = xml_rec.find('FromUserName').text
+        # content = xml_rec.find('Content').text
+        content = getContent(fromu)
+        xml_rep = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        response = make_response(xml_rep % (fromu,tou,str(int(time.time())), content))
+        response.content_type='application/xml'
+        return response
+
 
 if __name__ == "__main__":
 
